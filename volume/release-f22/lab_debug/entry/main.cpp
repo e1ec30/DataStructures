477f257cf74ffc8cffac15cd63c51125bb042c7f
e1ec30 <thelastelectro@gmail.com>
2025-12-02 19:01:55 +0100

    LabDebug: Started

--- volume/release-f22/lab_debug/entry/main.cpp
+++ volume/release-f22/lab_debug/entry/main.cpp
@@ -9,6 +9,8 @@ int main() {
     // If you get PNG Decoder error, it is because there is no such PNG "in.png".
     // Kindly change the input to one of the inputs you want to test in order to
     // make it work (ie. in_01.png)
-    sketchify("../tests/in.png", "../tests/out.png");
+    std::string in{"../tests/in.png"};
+    std::string out{"../tests/out.png"};
+    sketchify(in, out);
     return 0;
 }
